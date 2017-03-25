c8fe822a4ed3b4ab302fb41c584dbbd724f039c2
Keren <keren.luzon@mail.huji.ac.il>
2017-03-25 17:08:22 +0300

    v2 commit - added date piker and call dialer for task in the following format: "call 054-666-7777"

--- app/src/main/java/com/kerenlu/mytodolistapplication/MainActivity.java
+++ app/src/main/java/com/kerenlu/mytodolistapplication/MainActivity.java
@@ -1,8 +1,13 @@
 package com.kerenlu.mytodolistapplication;
 
 import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -11,7 +16,10 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -36,15 +44,30 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final DatePickerFragment newFragment = new DatePickerFragment();
+                LinearLayout layout = new LinearLayout(view.getContext());
                 final EditText taskEditText = new EditText(view.getContext());
+                taskEditText.setHint("What do you want to do next?");
+                layout.addView(taskEditText);
+                final ImageButton calendarButton = new ImageButton(view.getContext());
+                Resources res = getResources(); // need this to fetch the drawable
+                Drawable draw = res.getDrawable(android.R.drawable.ic_menu_my_calendar);
+                calendarButton.setImageDrawable(draw);
+                calendarButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        newFragment.show(getSupportFragmentManager(), "datePicker");
+                    }
+                });
+                layout.addView(calendarButton);
                 AlertDialog dialog = new AlertDialog.Builder(view.getContext())
                         .setTitle("Add a new task")
-                        .setMessage("What do you want to do next?")
-                        .setView(taskEditText)
+                        .setView(layout)
                         .setPositiveButton("Add", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 String task = String.valueOf(taskEditText.getText());
+                                task += " - " + newFragment.getDate();
                                 Log.d(TAG, "Task to add: " + task);
                                 addArray.add(task);
                                 ArrayAdapter<String> adapter = new MyAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, addArray);
--- app/src/main/java/com/kerenlu/mytodolistapplication/MyAdapter.java
+++ app/src/main/java/com/kerenlu/mytodolistapplication/MyAdapter.java
@@ -1,15 +1,24 @@
 package com.kerenlu.mytodolistapplication;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static android.R.id.list;
 
@@ -50,6 +59,37 @@ class MyAdapter<String> extends ArrayAdapter<String> {
                         .create();
                 dialog.show();
                 return true;
+            }
+        });
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final java.lang.String task = MyAdapter.this.getItem(position).toString();
+                Button callButton = null;
+                java.lang.String phoneNum = null;
+                if (task.contains("call")) {
+                    Pattern pattern = Pattern.compile("\\d{3}-\\d{3}-\\d{4}");
+                    Matcher matcher = pattern.matcher(task);
+                    if (matcher.find()) {
+                        phoneNum = matcher.group(0);
+                    }
+                    callButton = new Button(view.getContext());
+                    callButton.setHint(phoneNum);
+                    final java.lang.String parsePhoneNum = "tel:" + phoneNum;
+                    callButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Intent dial = new Intent(Intent.ACTION_DIAL, Uri.parse(parsePhoneNum));
+                            view.getContext().startActivity(dial);
+                        }
+                    });
+                }
+                AlertDialog dialog = new AlertDialog.Builder(view.getContext())
+                        .setTitle(task)
+                        .setView(callButton)
+                        .create();
+                dialog.show();
             }
         });
 
